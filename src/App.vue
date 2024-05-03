a7c9c84247788ece67b17234120f1e27292204e1
AjeshMadhavan <ajeshmadhavanncas@gmail.com>
2024-05-03 10:12:44 +0530

    quiz and todo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <QuizParent/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuizParent from './components/quizApp/QuizParent.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {  QuizParent}
 }
 </script>
 
 <style>
+html, body, *{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
